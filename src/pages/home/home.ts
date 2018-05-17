acf9d57b2cb86180700e7240b80efeff909822a5
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2018-05-17 21:42:10 +0700

    fix validate name & price

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,43 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  myGroup : FormGroup;
+  myGroup: FormGroup;
   nameItem: Object[];
-  isCheckListItem:boolean;
-  isCheckDeleteUpdate:boolean;
-  constructor(public navCtrl: NavController,private httpClient:HttpClient) {
+  isCheckListItem: boolean;
+  isCheckDeleteUpdate: boolean;
+  constructor(public navCtrl: NavController, private httpClient: HttpClient) {
     this.myGroup = new FormGroup({
-      name: new FormControl(),
-      price: new FormControl()
-   });    
+      name: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required)
+    });
   }
 
   addItem() {
-   
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type':  'application/json',
-    //     'Authorization': 'my-auth-token'
-    //   })
-    // };
-    return new Promise(resolve => {
-      const url = 'http://localhost:1323/sevens'
-      let body = new FormData();
-      body.append('name',this.myGroup.get('name').value);
-      body.append('price',this.myGroup.get('price').value);
-      this.httpClient.post(url,body).subscribe(data => {
-        // resolve(data);
-        console.log(data);
-      }, err => {
-        console.log(err);
+    if (this.myGroup.valid) {
+      return new Promise(resolve => {
+        const url = 'http://localhost:1323/sevens'
+        let body = new FormData();
+        body.append('name', this.myGroup.get('name').value);
+        body.append('price', this.myGroup.get('price').value);
+        this.httpClient.post(url, body).subscribe(data => {
+          console.log(data);
+        }, err => {
+          console.log(err);
+        });
       });
-    });
+    }
+    this.myGroup.get('name').markAsTouched();
+    this.myGroup.get('price').markAsTouched();
+
   }
 
   clear() {
@@ -46,8 +43,8 @@ export class HomePage {
     this.isCheckListItem = false;
 
   }
-  
-  getItemlist(){
+
+  getItemlist() {
     return new Promise(resolve => {
       const url = 'http://localhost:1323/sevens'
       this.httpClient.get(url).subscribe(data => {
@@ -61,10 +58,10 @@ export class HomePage {
     });
   }
 
-  deleteItem(id:string){
+  deleteItem(id: string) {
     return new Promise(resolve => {
       const url = 'http://localhost:1323/sevens/'
-      this.httpClient.delete(url+id).subscribe(data => {
+      this.httpClient.delete(url + id).subscribe(data => {
         this.isCheckListItem = false;
         console.log(data);
       }, err => {
